db633a9046a7dff8989797461559a9aaa41e7b4b
Gas111 <gastoncolque@gmail.com>
2023-04-03 22:02:40 -0300

    feat:optimice axios

--- ecommerce-tech/src/pages/LoginScreen.jsx
+++ ecommerce-tech/src/pages/LoginScreen.jsx
@@ -15,41 +15,58 @@ import Header from '../components/shared/Header'
 import './styles/loginscreen.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { setIsLogged } from '../store/slices/isLogged.slice'
+import LoadingAnimation from '../components/shared/LoadingAnimation'
+import { setUnitsCart } from '../store/slices/quantityCart.slice'
 
 const LoginScreen = ({}) => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const [isLoading, setIsLoading] = useState(false)
+
   const isLogged = useSelector((state) => state.isLogged)
   const { handleSubmit, register, reset } = useForm()
   const [errorLogged, setErrorLogged] = useState(false)
 
-  useEffect(() => {}, [isLogged, errorLogged])
+  useEffect(() => {
+
+  
+  }, [isLogged, errorLogged])
 
   const submit = (data) => {
     const URL = 'https://e-commerce-api.academlo.tech/api/v1/users/login'
 
+    setIsLoading(true)
     axios
       .post(URL, data)
       .then((res) => {
         localStorage.setItem('token', res.data.data.token)
         setErrorLogged(false)
         dispatch(setIsLogged(true))
+        setIsLoading(false)
+       
+
       })
       .catch((err) => {
+        setIsLoading(false)
         setErrorLogged(true)
         dispatch(setIsLogged(false))
         reset()
       })
+
+    
+
+
+
   }
 
   const handleIsLogged = () => {
     localStorage.removeItem('token')
-    setUnitsInCart(0)
+    dispatch(setUnitsCart(0))
     dispatch(setIsLogged(false))
     navigate('/login')
     reset()
+  
   }
-
   if (localStorage.getItem('token')) {
     return (
       <main className="user-logged">
@@ -62,27 +79,38 @@ const LoginScreen = ({}) => {
     )
   }
 
+
+
+
   return (
     <main className="main-login">
       <Header />
-      <form onSubmit={handleSubmit(submit)}>
-        <h2>Welcome Enter your email and password to continue</h2>
-        <p>For test use gastoncolque@gmail.com</p>
-        <p>and password: abc123456789</p>
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="text" id="email" {...register('email')} />
-        </div>
-        <div>
-          <label htmlFor="">Password</label>
-          <input type="text" id="password" {...register('password')} />
-        </div>
-        <div>
-          <p className="error">{errorLogged && 'error in email or password'}</p>
-        </div>
 
-        <button className="main-button">Login</button>
-      </form>
+      {isLoading ? (
+        <LoadingAnimation />
+      ) : (
+        <form onSubmit={handleSubmit(submit)}>
+          <h2>Welcome Enter your email and password to continue</h2>
+          <p>For test use gastoncolque@gmail.com</p>
+          <p>and password: abc123456789</p>
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="text" id="email" {...register('email')} />
+          </div>
+          <div>
+            <label htmlFor="">Password</label>
+            <input type="text" id="password" {...register('password')} />
+          </div>
+          <div>
+            <p className="error">
+              {errorLogged && 'error in email or password'}
+            </p>
+          </div>
+
+          <button className="main-button">Login</button>
+        </form>
+      )}
+
       <hr />
     </main>
   )
